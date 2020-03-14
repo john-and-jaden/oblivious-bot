8c401e9e8b05a4a0a5eb4f2e7942d28be9de256e
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-03-14 01:40:06 -0700

    clean up server.js file

--- server.js
+++ server.js
@@ -1,74 +1,60 @@
+// general imports
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+
+// faceapi specific imports
+// implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
+const canvas = require('canvas');
+const faceapi = require('face-api.js');
+
+// configure server
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send(`GET: you said ${req.query.param}`);
-});
-
-// import nodejs bindings to native tensorflow,
-// not required, but will speed up things drastically (python required)
-require('@tensorflow/tfjs-node');
-
-// implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
-const canvas = require('canvas');
-
-const faceapi = require('face-api.js');
-
 // patch nodejs environment, we need to provide an implementation of
 // HTMLCanvasElement and HTMLImageElement
 const { Canvas, Image, ImageData, createCanvas } = canvas;
 
-setup();
+setupFaceApiRequirements();
 
 app.post('/', (req, res) => {
-  processImage(req.body.param);
-  //   console.log('hey');
-  //   res.send(
-  //     'You said ' +
-  //       req.body.param +
-  //       '\n And I respond the reverse: ' +
-  //       myFun(req.body.param)
-  //   );
+  const results = getExpressionsFromImage(req.body.base64);
+  res.send(results);
 });
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}! 🚀`)
 );
 
-function myFun(input) {
-  result = '';
-  for (var i = input.length - 1; i >= 0; i -= 1) {
-    result += input[i];
-  }
-  return result;
-}
-
-async function setup() {
-  faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
-
-  await faceapi.nets.tinyFaceDetector.loadFromDisk('./models');
-  await faceapi.nets.faceExpressionNet.loadFromDisk('./models');
-}
-
-async function processImage(base64Input) {
+async function getExpressionsFromImage(base64Input) {
+  // retrieve and draw image
   const canvas = createCanvas(500, 500);
   const ctx = canvas.getContext('2d');
-
   const img = new Image();
   img.onload = () => ctx.drawImage(img, 0, 0);
   img.onerror = err => {
     throw err;
   };
-
   img.src = 'data:image/png;base64,' + base64Input;
 
+  // download image
   const buffer = canvas.toBuffer('image/png');
-  fs.writeFileSync('test.png', buffer);
+  fs.writeFileSync('imageFromUser.png', buffer);
+}
+
+async function setupFaceApiRequirements() {
+  // import nodejs bindings to native tensorflow,
+  // not required, but will speed up things drastically (python required)
+  require('@tensorflow/tfjs-node');
+
+  faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+
+  // load models
+  await faceapi.nets.tinyFaceDetector.loadFromDisk('./models');
+  await faceapi.nets.faceExpressionNet.loadFromDisk('./models');
 }

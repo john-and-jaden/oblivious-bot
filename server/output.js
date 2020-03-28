fd8cbfb7cf3300b413eb84aa537367a39b08746a
JadenBalogh <jadenbalogh@gmail.com>
2020-03-27 21:05:39 -0700

    Update output.js

--- server/output.js
+++ server/output.js
@@ -6,17 +6,17 @@ exports.output = [{
   synonyms: ['happy', 'pleased', 'amused', 'hee hee xd', 'joyful']
 }, {
   expression: 'angry',
-  synonyms: ['infuriated', 'outraged', 'big mad', 'angery', 'you mad bro', 'calm down', 'take a deep breath']
+  synonyms: ['infuriated', 'outraged', 'big mad', 'angery', 'you mad, bro', 'calm down', 'take a deep breath', 'why you so mad fam']
 }, {
   expression: 'sad',
   synonyms: ['sad', 'depressed', 'a homie going through a lot right now', 'you good bro']
 }, {
   expression: 'disgusted',
-  synonyms: ['horrified', 'you good bro']
+  synonyms: ['horrified', 'disgusted']
 }, {
   expression: 'surprised',
   synonyms: ['shocked', 'surprised', 'stunned']
 }, {
   expression: 'fearful',
-  synonyms: ['scared', 'you good bro']
+  synonyms: ['scared']
 }];

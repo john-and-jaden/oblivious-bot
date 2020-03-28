cc19be885425688369aa2755f46e8fdbcff37f23
JadenBalogh <jadenbalogh@gmail.com>
2020-03-27 20:08:56 -0700

    Add angry outputs

--- server/output.js
+++ server/output.js
@@ -6,7 +6,7 @@ exports.output = [{
   synonyms: ['happy', 'pleased', 'amused', 'hee hee xd', 'joyful']
 }, {
   expression: 'angry',
-  synonyms: ['infuriated', 'outraged', 'big mad', 'angery', 'you good bro?']
+  synonyms: ['infuriated', 'outraged', 'big mad', 'angery', 'you mad bro', 'calm down', 'take a deep breath']
 }, {
   expression: 'sad',
   synonyms: ['sad', 'depressed', 'a homie going through a lot right now', 'you good bro']
